301dd1de6b4f5a5cd901529392bac7e133b33329
keinuma <nununu.mono@gmail.com>
2024-03-31 14:16:06 +0900

    [API]初マイグレーション

--- apps/api/prisma/migrations/20240331051523_create_users/migration.sql
+++ apps/api/prisma/migrations/20240331051523_create_users/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `key` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
 
-    UNIQUE INDEX `User_key_key`(`key`),
-    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `users_key_key`(`key`),
+    UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
