6ded80f6c927f8a6f69978d5acf1d87c56ac928e
keinuma <nununu.mono@gmail.com>
2024-03-31 14:35:58 +0900

    データベース内のデータをAPIから返す

---
+++ apps/api/src/controllers/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from '../services/app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  async getHello(): Promise<string> {
+    const users = await this.appService.findAll();
+    if (users.length === 0) {
+      return 'No users found';
+    }
+
+    const firstUser = users[0];
+    return `${firstUser.name}がみつかりました`;
+  }
+}
---
+++ apps/api/src/services/app.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { users } from '@prisma/client';
+
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class AppService {
+  constructor(private prisma: PrismaService) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+  findAll(): Promise<users[]> {
+    return this.prisma.users.findMany();
+  }
+}
